16955925802e5da70498e7ae19eb3f88e99f6a4d
Rakshapatil0407 <raksha.patil0407@gmail.com>
2025-03-29 18:18:51 +0530

    commited

---
+++ printing_statments.java
@@ -0,0 +1,12 @@
+public class{
+
+public static void main(String args[]){
+
+   System.out.println("hello java");
+   System.out.println("hello world");
+   System.out.println("programmers day");
+
+}
+
+
+}
